95314d9671813d3afb2a4658e8d5c5319880dd17
najahaboobackar <najahaboobacker79@gmail.com>
2024-05-03 01:24:13 +0530

    added postgres:

---
+++ database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+URL_DATABASE='postgresql://postgres:najah123@localhost:5432/QuizApplication'
+engine=create_engine(URL_DATABASE)
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+Base=declarative_base()
